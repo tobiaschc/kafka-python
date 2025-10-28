9c1994a77f09a4efb9e9535d4065f99cc9e40da7
Tobias Chavarria <tobias.chavarria.castro@gmail.com>
2025-10-29 00:18:39 +0100

    feat: send correlation ID

--- app/main.py
+++ app/main.py
@@ -7,7 +7,29 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept()  # wait for client
+    conn, addr = server.accept()
+    with conn:
+        print(f"Connected by {addr}")
+
+        request = conn.recv(1024)
+        print(f"Received request: {request}")
+
+        # Create response message
+        # message_size: 4 bytes (32-bit signed integer) - any value works for this stage
+        # correlation_id: 4 bytes (32-bit signed integer) - must be 7
+        message_size = (4).to_bytes(4, byteorder="big", signed=True)
+        correlation_id = (7).to_bytes(4, byteorder="big", signed=True)
+
+        response_msg = b"".join(
+            [
+                message_size,
+                correlation_id,
+            ]
+        )
+
+        print(f"Sending response: {response_msg}")
+
+        conn.sendall(response_msg)
 
 
 if __name__ == "__main__":
