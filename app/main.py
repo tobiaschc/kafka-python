9add1b00d59f817a241e132a9342449c63c31e87
Tobías Chavarría <tobias.chavarria.castro@gmail.com>
2025-10-31 22:37:37 +0100

    feat: parse correlation id

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+import struct
 
 
 def main():
@@ -12,24 +13,36 @@ def main():
         print(f"Connected by {addr}")
 
         request = conn.recv(1024)
+
         print(f"Received request: {request}")
 
-        # Create response message
-        # message_size: 4 bytes (32-bit signed integer) - any value works for this stage
-        # correlation_id: 4 bytes (32-bit signed integer) - must be 7
-        message_size = (4).to_bytes(4, byteorder="big", signed=True)
-        correlation_id = (7).to_bytes(4, byteorder="big", signed=True)
+        message_size = struct.unpack(">i", request[0:4])[0]
+        print(f"Unpacked message_size: {message_size}")
 
-        response_msg = b"".join(
-            [
-                message_size,
-                correlation_id,
-            ]
+        request_api_key = struct.unpack(">h", request[4:6])[0]
+        print(f"Unpacked request_api_key: {request_api_key}")
+
+        request_api_version = struct.unpack(">h", request[6:8])[0]
+        print(f"Unpacked request_api_version: {request_api_version}")
+
+        correlation_id = struct.unpack(">i", request[8:12])[0]
+        print(f"Unpacked correlation_id: {correlation_id}")
+
+        # Send back the correlation ID as a 4-byte big-endian integer
+        correlation_id_int32 = (correlation_id).to_bytes(
+            4, byteorder="big", signed=True
         )
+        print(f"Sending back correlation_id: {correlation_id_int32}")
 
-        print(f"Sending response: {response_msg}")
+        # Create the response
+        # message_size: 4 bytes (just the correlation_id field)
+        # correlation_id: 4 bytes with the value from the request
+        response_message_size = 4
+        response = struct.pack(">ii", response_message_size, correlation_id)
 
-        conn.sendall(response_msg)
+        print(f"Response message: {response}")
+
+        conn.sendall(response)
 
 
 if __name__ == "__main__":
